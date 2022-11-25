16b5757f92bce65b5c96f1507f1b129318fac624
Ubuntu <ubuntu@ip-172-31-30-185.eu-west-1.compute.internal>
2022-11-25 18:19:19 +0000

    Final

--- despliegues/infra-aws/main.tf
+++ despliegues/infra-aws/main.tf
@@ -6,22 +6,28 @@ resource "aws_instance" "foo" {
     tags = {
         Name = "InstanciaDeIvan"  
     }
+    security_groups = [ aws_security_group.permitir_todo.name ]
     
     // public_ip private_ip     Ping:       Pruebo que la maquina está on y conectada a red
     // conectar por ssh ...     Probamos:   El certificado o Puerto 
     //                          SI: Se que todo está bien
     //                          NO: No se qué pasa ¿?
     //                          Pruebas unitarias
-    
+    # Smoke test = Prueba de humo
     provisioner "local-exec" {
-    
+        command = "sleep 20 && ping -c 1 ${self.public_ip}"
+    }    
     
     connection {
-        
+        type        =  "ssh"
+        host        =   self.public_ip
+        port        =   22
+        user        =   "ubuntu"
+        private_key =   module.claves.privateKey.pem
     }
     
     provisioner "remote-exec" {
-        
+        inline      = [ "echo EUREKA " ]
     }
     
 }
@@ -35,6 +41,29 @@ resource "aws_key_pair" "clave_publica_en_aws" {
     key_name    = "clave_publica_ivan"
     public_key  = module.claves.publicKey.openssh
 }
+
+resource "aws_security_group" "permitir_todo" {
+  name        = "reglas_firewall_ivan"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "reglas_firewall_ivan"
+  }
+}
+
 
 data "aws_ami" "imagen_ubuntu" {
   most_recent      = true
@@ -55,3 +84,39 @@ data "aws_ami" "imagen_ubuntu" {
     values = ["hvm"]
   }
 }
+
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+/*
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.my_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+  
+*/
--- notas/dia5.md
+++ notas/dia5.md
@@ -4,3 +4,17 @@ script      --->    terraform   ---> init   ---> descargar el proveedor
 declaramos                      ---> apply  --->  proveedor aws ------> aws cli ----->  aws
 un proveedor
 aws
+
+---
+## Control del estado de la infraestructura
+
+terraform refresh                       Refresca con el proveedor
+terraform show                          Muestra todos los recursos con toda su info
+terraform state list                    Muestra solo los nombres de recursos
+terraform state show aws_instance.foo   Muestra los datos de un recurso
+
+## Grafo de dependencias
+
+terraform graph -type=plan | dot -Tsvg > infra.svg
+terraform graph -type=plan-destroy | dot -Tsvg > infra.svg
+terraform graph -type=apply | dot -Tsvg > infra.svg
