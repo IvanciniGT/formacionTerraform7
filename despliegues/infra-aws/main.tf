cd6eec29802e56e949c253f53ddc85d784805eab
Ubuntu <ubuntu@ip-172-31-30-185.eu-west-1.compute.internal>
2022-11-25 17:28:56 +0000

    Instancia AWS

--- despliegues/infra-aws/main.tf
+++ despliegues/infra-aws/main.tf
@@ -1,12 +1,22 @@
 
 resource "aws_instance" "foo" {
-    ami           = data.aws_ami.imagen_ubuntu.image_id
-    instance_type = "t2.micro"
+    ami             = data.aws_ami.imagen_ubuntu.image_id
+    instance_type   = "t2.micro"
+    key_name        = resource.aws_key_pair.clave_publica_en_aws.key_name
     tags = {
         Name = "InstanciaDeIvan"  
     }
 }
 
+module "claves" {
+    source              = "../modulo"
+    directorio_claves   = "./claves"
+}
+
+resource "aws_key_pair" "clave_publica_en_aws" {
+    key_name    = "clave_publica_ivan"
+    public_key  = module.claves.publicKey.openssh
+}
 
 data "aws_ami" "imagen_ubuntu" {
   most_recent      = true
